34414f6d86410afed5962c4625877c3ac7808431
Mila Zolotova <l.zolotova@yahoo.com>
2022-09-28 22:36:29 +0300

    edited components - Login & Header

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,5 +1,4 @@
 import './index.scss';
-import 'bootstrap';
 
 const Login = () => {
   return (
--- src/ui/Header/index.jsx
+++ src/ui/Header/index.jsx
@@ -2,7 +2,7 @@ import {NavLink} from "react-router-dom";
 
 import './index.scss';
 
-export const links = [
+const links = [
   {link: '/', label: 'Главная'},
   {link: '/login', label: 'Регистрация/ Вход'},
   {link: '*', label: '404'},
@@ -10,16 +10,11 @@ export const links = [
 
 const Header = () => {
   return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <nav>
-            {links.map(({link, label}) => <NavLink key={link} to={link} className="nav__item">{label}</NavLink>)}
-          </nav>
-        </Col>
-        <Col><Button variant="outline-light" size="lg">Выйти</Button>{' '}</Col>
-      </Row>
-    </Container>
+    <>
+      <nav>
+        {links.map(({link, label}) => <NavLink key={link} to={link} className="nav__item">{label}</NavLink>)}
+      </nav>
+    </>
   );
 }
 

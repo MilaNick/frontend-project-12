e2f0e13acb55e94d3081a76cdc988670340d4faa
Mila Zolotova <l.zolotova@yahoo.com>
2022-10-13 13:05:30 +0300

    getting started getting data from the server

--- src/screens/Main/index.jsx
+++ src/screens/Main/index.jsx
@@ -1,10 +1,15 @@
+import axios from "axios";
+
 import './index.scss';
 
 const Main = () => {
+  axios.get('/api/v1/data').then((response) => {
+    console.log(response.data); // => { channels: [...], currentChannelId: 1, messages: [] }
+  });
   return(
     <>
 
-      </>
+    </>
   )
 }
 

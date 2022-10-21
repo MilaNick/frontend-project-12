eaabe4f7fa22f52dfcf549c7bdd7e3332b2d39d7
Mila Zolotova <l.zolotova@yahoo.com>
2022-10-21 14:34:43 +0300

    added navigation to new channel

--- src/components/Channels/index.jsx
+++ src/components/Channels/index.jsx
@@ -1,4 +1,5 @@
-import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {useEffect, useState} from 'react';
 import {useSelector} from 'react-redux';
 
 import Channel from 'components/Channel';
@@ -12,15 +13,25 @@ const plus = '+';
 const Channels = ({activeChannelId}) => {
   const channels = useSelector((state) => state.channelsReducer.channels);
   const [shown, setShown] = useState(false);
+  const navigate = useNavigate();
+  const [justCreatedChannelName, setJustCreatedChannelName] = useState(null)
+  useEffect(() => {
+    if(justCreatedChannelName) {
+      const exist = channels.find(channel => channel.name === justCreatedChannelName)
+      if (exist) {
+        navigate(`/chats/${exist.id}`)
+        setJustCreatedChannelName(null)
+      }
+    }
+  }, [channels, justCreatedChannelName])
+
 
   return (
     <div className='main-channels'>
       <div className='main-channels__wrap'>
         <h3 className='main-channels__title'>Каналы</h3>
         <Button size='sm' onClick={() => setShown(true)}>{plus}</Button>
-        <CreateChannel channels={channels} shown={shown} setShown={setShown} />
-
-
+        <CreateChannel onAddChannel={setJustCreatedChannelName} channels={channels} shown={shown} setShown={setShown} />
       </div>
       <ul className='main-channels__names'>
         {channels.map((channel) => {
--- src/components/CreateChannel/index.jsx
+++ src/components/CreateChannel/index.jsx
@@ -8,38 +8,41 @@ import Form from 'ui/Form';
 import Input from 'ui/Input';
 import Popup from 'ui/Popup';
 
-const CreateChannel = ({channels, shown, setShown}) => {
+const CreateChannel = ({channels, shown, setShown, onAddChannel}) => {
   const [newChannel, setNewChannel] = useState('');
   const [error, setError] = useState('');
 
   const addChannel = (e) => {
     e.preventDefault();
-    if (channels.find(channel => Object.values(channel).includes(newChannel))) {
+    if (channels.find(channel => channel.name.toLowerCase() === newChannel.toLowerCase())) {
       setError('Канал с таким названием уже есть')
-      setNewChannel('');
       return;
     }
     setError('')
     socket.emit('newChannel', {name: newChannel}, (response) => {
       console.log(response.status); // TODO как обработать этот статус, куда вывести?
     })
+    onAddChannel(newChannel);
     setNewChannel('');
     setShown(false);
-  }; // TODO добавить переход на созданный канал
-
-  {/*TODO инкапсуляция попапом логики*/
+  };
+  const closePopup = () => {
+    setNewChannel('');
+    setError('')
+    setShown(false)
   }
+
   return (
     <Popup shown={shown} setShown={setShown}>
       <Form onSubmit={addChannel}>
         <div className='wrap'>
           <h3>Добавить канал</h3>
-          <Button size='sm' onClick={() => setShown(false)}>x</Button>
+          <Button size='sm' onClick={closePopup}>x</Button>
         </div>
         <Input value={newChannel} onChange={(e) => setNewChannel(e.target.value)}/>
         {error && <Report type='error'>{error}</Report>}
         <div className='wrapper'>
-          <Button size='lg' top='lg' left onClick={() => setShown(false)}>Отменить</Button>
+          <Button size='lg' top='lg' left onClick={closePopup}>Отменить</Button>
           <Button type='submit' size='lg' top='lg' left>Отправить</Button>
         </div>
       </Form>

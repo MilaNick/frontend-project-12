1b56f8a31c2e60df76fa8976a93b332353eca6b2
Mila Zolotova <l.zolotova@yahoo.com>
2022-10-24 01:20:29 +0300

    added component Icon

---
+++ src/ui/Icon/index.jsx
@@ -0,0 +1,18 @@
+import {ReactComponent as ArrowDown} from 'assets/images/svg/arrow-down.svg';
+import {ReactComponent as ArrowRight} from 'assets/images/svg/arrow-right.svg';
+import {ReactComponent as Close} from 'assets/images/svg/close.svg';
+import {ReactComponent as Plus} from 'assets/images/svg/plus.svg';
+
+import './index.scss';
+
+const icons = {ArrowDown, ArrowRight, Close, Plus};
+
+const Icon = ({icon}) => {
+  const ConcreteIcon = icons[icon];
+
+  return(
+    <ConcreteIcon className='icon'/>
+  )
+}
+
+export default Icon;

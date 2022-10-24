4435bd75fd85faa477e7a62235c94b4a67b8c58f
Mila Zolotova <l.zolotova@yahoo.com>
2022-10-25 00:28:38 +0300

    refactor Popup

--- src/ui/Popup/index.jsx
+++ src/ui/Popup/index.jsx
@@ -1,14 +1,28 @@
 import cn from 'classnames';
 
+import Button from 'ui/Button';
+import Icon from 'ui/Icon';
+
 import './index.scss';
 
-const Popup = ({children, shown, setShown}) => {
+const Popup = ({children, shown, close, title}) => {
   const classes = cn({
+    popup: true,
     'popup--shown': shown,
   })
+
+  const classesForContent = cn({
+    'popup__content': true,
+    'popup--shown': shown,
+  })
+
   return (
-    <div className={`popup ${classes}`} onClick={() => setShown(false)}>
-      <div className={`popup__content ${classes}`} onClick={(e) => e.stopPropagation()}>
+    <div className={classes} onClick={close}>
+      <div className={classesForContent} onClick={(e) => e.stopPropagation()}>
+        <div className='wrap'>
+          <h3>{title}</h3>
+          <Button size='sm' onClick={close}><Icon icon='Close'/></Button>
+        </div>
         {children}
       </div>
     </div>

480c2b3c0d2c0929a3a3e83c3e87ad8171ac3c9c
Mila Zolotova <l.zolotova@yahoo.com>
2022-10-25 01:14:09 +0300

    added new components: removeChannelPopup & renameChannelPopup

---
+++ src/components/RemoveChannelPopup/index.jsx
@@ -0,0 +1,28 @@
+import {socket} from 'index';
+
+import Button from 'ui/Button';
+import Popup from 'ui/Popup';
+
+const RemoveChannelPopup = ({id, shown, setShown}) => {
+
+  const deleteChannel = () => {
+    setShown(true);
+    socket.emit('removeChannel', {id});
+  }
+
+  const closePopup = () => {
+    setShown(false)
+  };
+
+  return (
+    <Popup shown={shown} close={closePopup} title='Удалить канал'>
+      <div className='wrapper'>
+        <div>Вы уверены?</div>
+        <Button size='lg' top='lg' left onClick={closePopup}>Отменить</Button>
+        <Button type='submit' size='lg' top='lg' left onClick={() => deleteChannel(id)}>Удалить</Button>
+      </div>
+    </Popup>
+  )
+}
+
+export default RemoveChannelPopup;
---
+++ src/components/RenameChannelPopup/index.jsx
@@ -0,0 +1,31 @@
+import {useState} from 'react';
+
+import {socket} from 'index';
+
+import Report from 'components/Report';
+import Button from 'ui/Button';
+import Form from 'ui/Form';
+import Input from 'ui/Input';
+import Popup from 'ui/Popup';
+
+const RenameChannelPopup = ({channels, shown, setShown, onAddChannel}) => {
+
+  const closePopup = () => {
+    setShown(false)
+  }
+
+  return (
+    <Popup shown={shown} close={closePopup} title='Добавить канал'>
+      <Form onSubmit={}>
+        <Input value={} onChange={}/>
+        {error && <Report type='error'>{error}</Report>}
+        <div className='wrapper'>
+          <Button size='lg' top='lg' left onClick={closePopup}>Отменить</Button>
+          <Button type='submit' size='lg' top='lg' left>Отправить</Button>
+        </div>
+      </Form>
+    </Popup>
+  )
+}
+
+export default RenameChannelPopup;

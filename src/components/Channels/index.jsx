052527c659a8a4e63b37e3407dd93833e8142f23
Mila Zolotova <l.zolotova@yahoo.com>
2022-11-07 22:49:04 +0300

    refactor

--- src/components/Channels/index.jsx
+++ src/components/Channels/index.jsx
@@ -5,7 +5,7 @@ import {useSelector} from 'react-redux';
 import Channel from 'components/Channel';
 import CreateChannelPopup from 'components/CreateChannelPopup';
 import Button from 'ui/Button';
-import Icon from 'ui/Icon';
+// import Icon from 'ui/Icon';
 
 import './index.scss';
 
@@ -29,7 +29,8 @@ const Channels = ({activeChannelId}) => {
     <div className='main-channels'>
       <div className='main-channels__wrap'>
         <h3 className='main-channels__title'>Каналы</h3>
-        <Button size='sm' onClick={() => setShown(true)}><Icon icon='Plus'/></Button>
+        <Button size='sm' onClick={() => setShown(true)}>+</Button>
+        {/*<Icon icon='Plus'/>*/}
         {shown && <CreateChannelPopup onAddChannel={setJustCreatedChannelName} channels={channels} setShown={setShown}/>}
       </div>
       <ul className='main-channels__names'>

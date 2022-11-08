cc66d873a369c11039d02ae6604c65b04e173f79
Mila Zolotova <l.zolotova@yahoo.com>
2022-11-08 14:36:14 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import React, { createContext, useState } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
